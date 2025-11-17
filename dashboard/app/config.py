d99c74513a2658e42c559a1d32812e7c5104db0c
jwlee <first202301@gmail.com>
2025-11-17 14:49:19 +0900

    2일 전 버전

--- dashboard/app/config.py
+++
@@ -1,8 +0,0 @@
-# app/config.py
-from pathlib import Path
-
-BASE_DIR = Path(__file__).parent.parent
-CONFIG_DIR = BASE_DIR / "config"
-CONFIG_DIR.mkdir(exist_ok=True)
-SERVERS_FILE = CONFIG_DIR / "servers.yaml"
-
